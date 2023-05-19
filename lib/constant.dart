9a0331fd15a3658f4c0baf3999b05b0b67d5d6b9
Afiq Harith <afiqharith15@gmail.com>
2023-05-19 11:26:22 +0800

    start

---
+++ lib/constant.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+const Color kScaffoldColor = Color(0xFF1C2833);
+const Color kPrimaryColor = Color(0xFF4A84C7);
+const Color kSecondaryColor = Color(0xFF2BB1EA);
+const Color kTextColor = Color(0xFFFFFFFF);
+const Color kTextColorShade = Color.fromARGB(199, 187, 184, 184);
+
+final kMargin = EdgeInsets.symmetric(horizontal: 2.w, vertical: 1.h);
+final kPadding = EdgeInsets.all(2.h);
+final kRadius = BorderRadius.circular(2.h);
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pointofsales/constant.dart';
+import 'package:pointofsales/widget/icon_btn.dart';
+import 'package:sizer/sizer.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "DASHBOARD",
+          style: GoogleFonts.ubuntu(
+            fontSize: 16.sp,
+            letterSpacing: 1.0,
+            fontWeight: FontWeight.w500,
+            color: kTextColor,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.notifications,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+            tooltip: "Notifications Section",
+          ),
+        ],
+      ),
+    );
+  }
+}
