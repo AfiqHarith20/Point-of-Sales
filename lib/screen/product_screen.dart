ee9514f99c8552b86c2502706e06a8b096ca400d
Afiq Harith <afiqharith15@gmail.com>
2023-07-07 17:28:34 +0800

    update

--- lib/screen/product_screen.dart
+++ lib/screen/product_screen.dart
@@ -22,88 +22,99 @@ class ProductScreen extends StatefulWidget {
 
 class _ProductScreenState extends State<ProductScreen> {
   bool isLoading = true;
-  List<ProductDetail> products = [];
+  late List<ProductDetail> products = [];
 
-  int? id, quantity, status, categoryId;
-  String? name, summary, sku, details;
-  double? price;
-  dynamic? isSearch, ispromo, promoPrice, promoStartdate, promoEnddate, stock, mainImage;
+  Future<List<ProductDetail>> fetchProduct() async {
+    final url = Uri.parse(Constants.apiProductIndex);
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final http.Response response = await http.get(
+      url,
+      headers: ({
+        'Authorization': 'Bearer ' + prefs.getString('token').toString(),
+        'Content-Type': 'application/json'
+      }),
+    );
 
-  Future<void> _getIndexProduct() async{
-    try{
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> json = jsonDecode(response.body);
+      final prod = Products.fromJson(json);
+      return prod.products;
+    } else {
+      throw Exception('Failed to fetch products');
+    }
+  }
+
+  void _getIndexProduct() async {
+    try {
       final SharedPreferences prefs = await SharedPreferences.getInstance();
-    var response = await http.get(Uri.parse(Constants.apiProductIndex),
-    headers: {
+      var response = await http.get(
+        Uri.parse(Constants.apiProductIndex),
+        headers: {
           'Authorization': 'Bearer ' + prefs.getString('token').toString(),
           'Content-Type': 'application/json'
-        }
-    );
-    print(response.body);
-    final Map<String, dynamic> prod = json.decode(response.body);
-    if (response.statusCode == 200) {
-      print("PRODUCT LIST >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-      setState(() {
-        isLoading = false;
+        },
+      );
+      print(response.body);
+      final Map<String, dynamic> prod = json.decode(response.body);
+      if (response.statusCode == 200) {
+        print("PRODUCT LIST >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        setState(() {
+          isLoading = false;
 
-        if (prod['data'] != null) {
-            var productsJson = prod['data'][0]['products'] as List<dynamic>;
-            products = productsJson
-                .map((productJson) => ProductDetail.fromJson(productJson))
-                .toList();
+          if (prod['data'] != null && prod['data'].isNotEmpty) {
+            var productsJson = prod['data'][0]['products'] as List<dynamic>?;
+            if (productsJson != null) {
+              products = productsJson
+                  .map((productJson) => ProductDetail.fromJson(productJson))
+                  .toList();
 
-            print(products);
+              Future<List<ProductDetail>> _productDetail = fetchProduct();
 
-          var productJson = prod['data'][0]['products'];
-            var data = ProductDetail(
-              id: productJson['id'] ?? 0,
-              sku: productJson['sku'],
-              name: productJson['name'],
-              summary: productJson['summary'],
-              details: productJson['details'],
-              categoryId: productJson['category_id'],
-              price: productJson['price'],
-              quantity: productJson['quantity'],
-              isSearch: productJson['isSearch'],
-              ispromo: productJson['ispromo'],
-              promoPrice: productJson['promo_price'],
-              promoStartdate: productJson['promo_startdate'],
-              promoEnddate: productJson['promo_enddate'],
-              status: productJson['status'],
-              stock: productJson['stock'],
-              productCategory:
-                  ProductCategory.fromJson(productJson['product_category']),
-            );
-            id = data.id;
-            sku = data.sku;
-            name = data.name;
-            summary = data.summary;
-            details = data.details;
-            categoryId = data.categoryId;
-            price = data.price;
-            quantity = data.quantity;
-            isSearch = data.isSearch;
-            ispromo = data.ispromo;
-            promoPrice = data.promoPrice;
-            promoStartdate = data.promoStartdate;
-            promoEnddate = data.promoEnddate;
-            stock = data.stock;
-            status = data.status;
-            mainImage = data.mainImage;
-            
-        } 
-      });
-    }} catch (e) {
+              print(products);
+
+              _productDetail.then((productDetail) {
+                setState(() {
+                  
+                  products = productDetail.map((productJson) {
+                    return ProductDetail(
+                      id: productJson.id,
+                      sku: productJson.sku,
+                      name: productJson.name,
+                      summary: productJson.summary,
+                      details: productJson.details,
+                      categoryId: productJson.categoryId,
+                      price: productJson.price,
+                      quantity: productJson.quantity,
+                      isSearch: productJson.isSearch,
+                      ispromo: productJson.ispromo,
+                      promoPrice: productJson.promoPrice,
+                      promoStartdate: productJson.promoStartdate,
+                      promoEnddate: productJson.promoEnddate,
+                      status: productJson.status,
+                      stock: productJson.stock,
+                      mainImage: productJson.mainImage,
+                      productCategory:
+                          ProductCategory.fromJson(productJson.productCategory as Map<String, dynamic>),
+                    );
+                    
+                  }).toList();
+                  
+                });
+              });
+            }
+          }
+        });
+      }
+    } catch (e) {
       print(e);
       setState(() {
         isLoading = false;
       });
-      
     }
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _getIndexProduct();
   }
@@ -158,85 +169,86 @@ class _ProductScreenState extends State<ProductScreen> {
         ],
       ),
       body: isLoading
-        ? Center(
-            child: GFLoader(type: GFLoaderType.ios),
-          )
-          :
-          products.isEmpty
-            ? Center(
-                child: Text('No products available', style: GoogleFonts.ubuntu(
+          ? Center(
+              child: GFLoader(type: GFLoaderType.ios),
+            )
+          : products.isEmpty
+              ? Center(
+                  child: Text(
+                    'No products available',
+                    style: GoogleFonts.ubuntu(
                       fontSize: 16.sp,
                       letterSpacing: 1.0,
                       fontWeight: FontWeight.w500,
                       color: kTextColor,
                     ),
                   ),
-              )
-          : SingleChildScrollView(
-              child: Column(
-                children: products
-                    .map(
-                      (product) => Container(
-                        margin: EdgeInsets.all(10.0),
-                        padding: EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 2,
-                              blurRadius: 5,
-                              offset: Offset(0, 3),
+                )
+              : SingleChildScrollView(
+                  child: Column(
+                    children: products
+                        .map(
+                          (product) => Container(
+                            margin: EdgeInsets.all(10.0),
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 2,
+                                  blurRadius: 5,
+                                  offset: Offset(0, 3),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                        child: Column(
-                          children: [
-                            product.mainImage != null &&
-                                    product.mainImage.isNotEmpty
-                                ? Image.network(
-                                    product.mainImage!,
-                                    height: 100.0,
-                                    width: 100.0,
-                                    fit: BoxFit.contain,
-                                  )
-                                : Image.asset(
-                                    'assets/apple.png',
-                                    height: 100.0,
-                                    width: 100.0,
-                                    fit: BoxFit.contain,
+                            child: Column(
+                              children: [
+                                product.mainImage != null &&
+                                        product.mainImage.isNotEmpty
+                                    ? Image.network(
+                                        product.mainImage!,
+                                        height: 100.0,
+                                        width: 100.0,
+                                        fit: BoxFit.contain,
+                                      )
+                                    : Image.asset(
+                                        'assets/apple.png',
+                                        height: 100.0,
+                                        width: 100.0,
+                                        fit: BoxFit.contain,
+                                      ),
+                                SizedBox(height: 10.0),
+                                Text(
+                                  product.name,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.0,
                                   ),
-                            SizedBox(height: 10.0),
-                            Text(
-                              product.name,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16.0,
-                              ),
+                                ),
+                                SizedBox(height: 5.0),
+                                Text(
+                                  '\RM${product.price.toStringAsFixed(2)}',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14.0,
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                                SizedBox(height: 10.0),
+                                Text(
+                                  product.summary,
+                                  style: TextStyle(
+                                    fontSize: 14.0,
+                                  ),
+                                ),
+                              ],
                             ),
-                            SizedBox(height: 5.0),
-                            Text(
-                              '\$${product.price.toStringAsFixed(2)}',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 14.0,
-                                color: Colors.blue,
-                              ),
-                            ),
-                            SizedBox(height: 10.0),
-                            Text(
-                              product.summary,
-                              style: TextStyle(
-                                fontSize: 14.0,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    )
-                    .toList(),
-              ),
-            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
     );
   }
 }
